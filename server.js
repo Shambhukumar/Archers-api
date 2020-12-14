d0fd1404a7b2feb6b631e5d8582581927364f7c2
shambhu kumar <shambhukumar.shau@gmail.com>
2020-12-14 09:24:19 +0530

    Some Changes

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const webscraping = require("./webscraping")
 const topStories = require("./controller/topStories");
 app.use(cors())
 app.use(express.json());
-
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers", "Origin, x-Requested-With, Content-Type, Accept")
+  next();
+});
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE, {useNewUrlParser: true});
